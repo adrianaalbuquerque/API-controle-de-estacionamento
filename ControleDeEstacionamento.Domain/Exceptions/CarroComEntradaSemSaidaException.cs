1b63d551e934f505619ed70e5b4a3947cea49968
Adriana Albuquerque <80019208+adrianaalbuquerque@users.noreply.github.com>
2025-09-16 15:28:52 -0300

    feat: adicionadas exceptions personalizadas para placa inválida e entrada sem saída

---
+++ ControleDeEstacionamento.Domain/Exceptions/CarroComEntradaSemSaidaException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeEstacionamento.Domain.Exceptions
+{
+    public class CarroComEntradaSemSaidaException : Exception
+    {
+        public CarroComEntradaSemSaidaException(string placa)
+        : base($"Não é possível realizar nova entrada para o carro. Carro com placa '{placa}' ainda se encontra no estacionamento.") { }
+    }
+}
---
+++ ControleDeEstacionamento.Domain/Exceptions/PlacaInvalidaException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeEstacionamento.Domain.Exceptions
+{
+    public class PlacaInvalidaException : Exception
+    {
+        public PlacaInvalidaException(string placa)
+            : base($"Placa '{placa}' inválida. Verifique o formato.") { }
+    }
+}
