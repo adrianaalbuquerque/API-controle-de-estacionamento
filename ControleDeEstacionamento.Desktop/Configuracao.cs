1ec97f5fd5a9e53ba463c3e5c196f0e6e6379817
Adriana Albuquerque <80019208+adrianaalbuquerque@users.noreply.github.com>
2025-09-17 10:19:41 -0300

    feat: adiciona arquivo configuracao para parametrizar url base da api

---
+++ ControleDeEstacionamento.Desktop/Configuracao.cs
@@ -0,0 +1,7 @@
+namespace ControleDeEstacionamento.Desktop
+{
+    public static class Configuracao
+    {
+        public const string ESTACIONAMENTO_URL_BASE = "http://localhost:5189/api";
+    }
+}
